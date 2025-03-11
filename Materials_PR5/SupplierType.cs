7a994d64c759040d9bcc8744ca9cd6a74cd8f9d0
Алексей Леонтьев <leontev.leha06@bk.ru>
2025-03-11 11:01:41 +0500

    Добавьте файлы проекта.

---
+++ Materials_PR5/SupplierType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Materials_PR5;
+
+public partial class SupplierType
+{
+    public short Id { get; set; }
+
+    public string TypeSupplier { get; set; } = null!;
+
+    public virtual ICollection<Supplier> Suppliers { get; set; } = new List<Supplier>();
+}
